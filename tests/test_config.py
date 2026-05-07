7b759f0ac1da31f60b162ba1a58923d7612446fb
Sani Patel <55621261+Sani192@users.noreply.github.com>
2026-05-07 17:16:23 +0530

    Create test_config.py

---
+++ tests/test_config.py
@@ -0,0 +1,12 @@
+ROLES = [
+    "chairman",
+    "secretary",
+    "treasurer",
+    "committee_member",
+    "non_committee_member",
+    "outsider"
+]
+
+EVENT_STATES = ["active", "expired", "none"]
+
+LANGUAGES = ["en", "hi", "gu"]
